6985ca8769461bfbb30caf38ca94a61b5c4b068d
Paw565pl <paw565pl@gmail.com>
2023-10-10 14:47:16 +0200

    feat 2: added pesel to account class

--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -5,10 +5,12 @@ from ..Konto import Konto
 class TestCreateBankAccount(unittest.TestCase):
     imie = "Dariusz"
     nazwisko = "Januszewski"
+    pesel = 12345678900
 
     def test_tworzenie_konta(self):
-        pierwsze_konto = Konto(self.imie, self.nazwisko)
+        pierwsze_konto = Konto(self.imie, self.nazwisko, self.pesel)
         self.assertEqual(pierwsze_konto.imie, self.imie, "Imie nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, self.nazwisko, "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
+        self.assertEqual(pierwsze_konto.pesel, self.pesel, "Pesel nie został zapisany!")
 
