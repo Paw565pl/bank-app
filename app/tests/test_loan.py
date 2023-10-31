408d16b7aead50d30ddfb15ca61f74484836247d
Paw565pl <paw565pl@gmail.com>
2023-10-31 15:31:33 +0100

    refactored test loan with parameterized

--- app/tests/test_loan.py
+++ app/tests/test_loan.py
@@ -1,5 +1,7 @@
 import unittest
 
+from parameterized import parameterized
+
 from ..CompanyBankAccount import CompanyBankAccount
 from ..PrivateBankAccount import PrivateBankAccount
 
@@ -11,22 +13,24 @@ class TestLoan(unittest.TestCase):
         "pesel": "94031633999",
     }
 
-    def test_loan_private_account_conditions_are_not_met_no_loan(self):
-        account = PrivateBankAccount(*self.person.values())
-        was_given = account.take_loan(1000)
-        self.assertFalse(was_given, "Loan was given!")
-        self.assertEqual(account.balance, 0, "Balance was changed!")
+    def setUp(self) -> None:
+        self.account = PrivateBankAccount(*self.person.values())
 
-    def test_loan_private_account_last_three_transfers_incoming(self):
-        account = PrivateBankAccount(*self.person.values())
-        account.transfer_history = [100, 100, 100]
-        was_given = account.take_loan(1000)
-        self.assertTrue(was_given, "Loan was not given!")
-        self.assertEqual(account.balance, 1000, "Balance was not changed!")
-
-    def test_loan_private_account_sum_of_transfers(self):
-        account = PrivateBankAccount(*self.person.values())
-        account.transfer_history = [-200, 100, 500, 700, -50]
-        was_given = account.take_loan(1000)
-        self.assertTrue(was_given, "Loan was not given!")
-        self.assertEqual(account.balance, 1000, "Balance was not changed!")
+    @parameterized.expand(
+        [
+            ([], 1000, False, 0),
+            ([100, 100, 100], 1000, True, 1000),
+            ([-200, 100, 500, 700, -50], 1000, True, 1000),
+        ]
+    )
+    def test_taking_loan(
+        self,
+        transfer_hsitory: list[int],
+        amount: int,
+        expected_result: bool,
+        expected_balance: int,
+    ) -> None:
+        self.account.transfer_history = transfer_hsitory
+        was_given = self.account.take_loan(amount)
+        self.assertEqual(was_given, expected_result)
+        self.assertEqual(self.account.balance, expected_balance)
