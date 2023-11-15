c967ec556bad9e088ca6914f11ac4197a2901044
Paw565pl <paw565pl@gmail.com>
2023-11-15 12:48:42 +0100

    feat 15: added test for count endpoint

--- app/api_test/account_crud.py
+++ app/api_test/account_crud.py
@@ -4,7 +4,6 @@ import requests
 
 
 class TestAccountCrud(unittest.TestCase):
-
     person = {
         "first_name": "Dariusz",
         "last_name": "Januszewski",
@@ -14,20 +13,31 @@ class TestAccountCrud(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.url = "http://localhost:5000/api/accounts"
-        
-    def test_create_account(self):
+
+    def test_1_create_account(self):
         response = requests.post(self.url, json=self.person)
         self.assertEqual(response.status_code, 201, "Account was not created!")
 
-    def test_get_account_account_exists(self):
-        response = requests.get(f"{self.url}/{self.person["pesel"]}")
-        self.assertEqual(response.status_code, 200, "Invalid status code!")
-
+    def test_accounts_count(self):
+        response = requests.get(f"{self.url}/count")
         data = response.json()
-        self.assertEqual(data["first_name"], self.person["first_name"], "Invalid first name!")
-        self.assertEqual(data["last_name"], self.person["last_name"], "Invalid last name!")
-        self.assertEqual(data["pesel"], self.person["pesel"], "Invalid pesel!")
+
+        self.assertEqual(response.status_code, 200, "Invalid status code!")
+        self.assertEqual(data["count"], 1, "Invalid accounts count!")
 
     def test_get_account_account_does_not_exists(self):
         response = requests.get(f"{self.url}/11111111111")
         self.assertEqual(response.status_code, 404, "Invalid status code!")
+
+    def test_get_account_account_exists(self):
+        response = requests.get(f"{self.url}/{self.person["pesel"]}")
+        data = response.json()
+
+        self.assertEqual(response.status_code, 200, "Invalid status code!")
+        self.assertEqual(
+            data["first_name"], self.person["first_name"], "Invalid first name!"
+        )
+        self.assertEqual(
+            data["last_name"], self.person["last_name"], "Invalid last name!"
+        )
+        self.assertEqual(data["pesel"], self.person["pesel"], "Invalid pesel!")
