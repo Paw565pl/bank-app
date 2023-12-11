a5166a94fa6788dfaacd560b3539b9eea0e5f5d0
Paw565pl <paw565pl@gmail.com>
2023-12-11 19:41:27 +0100

    changed parameter name

--- app/SMTPConnection.py
+++ app/SMTPConnection.py
@@ -1,5 +1,5 @@
 class SMTPConnection:
-    def send(self, subject: str, content: str, receiver: str) -> bool:
+    def send(self, subject: str, content: str, receiver_email: str) -> bool:
         # implementacja ktorej jeszcze nie mamy
         # return True jezeli wyslanie sie powiodło
         # return False jezeli wyslanie sie nie powiodło
