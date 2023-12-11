08d3d3681b9ba814c83d27e70cce084d4a301b6c
Paw565pl <paw565pl@gmail.com>
2023-12-11 19:33:39 +0100

    created smtp connection class

---
+++ app/SMTPConnection.py
@@ -0,0 +1,6 @@
+class SMTPConnection:
+    def send(self, subject: str, content: str, receiver: str) -> bool:
+        # implementacja ktorej jeszcze nie mamy
+        # return True jezeli wyslanie sie powiodło
+        # return False jezeli wyslanie sie nie powiodło
+        return False
